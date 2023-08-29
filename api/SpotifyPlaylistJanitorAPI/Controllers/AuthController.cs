c95c3279f1bd56035696380145a61bcb847ad4b6
PMcCarronUno <75257450+PMcCarronUno@users.noreply.github.com>
2023-08-29 10:54:23 +0100

    Create api project (#4)
    * Added ASP Net Core project
    
    * Renamed file
    
    * Added auth and example controllers
    Added api to docker compose
    
    * Added docker compose support
    
    * Remved volume from docker compose
    
    ---------
    
    Co-authored-by: pmccarron <pmccarron@foundationmedicine.com>

---
+++ api/SpotifyPlaylistJanitorAPI/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SpotifyPlaylistJanitorAPI.Models;
+using System.Diagnostics;
+
+namespace SpotifyPlaylistJanitorAPI.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+        private static string STATE = $"{Guid.NewGuid()}{Guid.NewGuid()}".Replace("-", "");
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult Auth()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult Callback()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
