85861af3c6c52a4c593a356db9fc153d275759ce
pmccarron <pmccarron@foundationmedicine.com>
2023-09-08 11:23:21 +0100

    Added more xml comments

--- api/SpotifyPlaylistJanitorAPI/Models/Spotify/SpotifyPlayingState.cs
+++ api/SpotifyPlaylistJanitorAPI/Models/Spotify/SpotifyPlayingState.cs
@@ -1,8 +1,18 @@
 ﻿namespace SpotifyPlaylistJanitorAPI.Models.Spotify
 {
+    /// <summary>
+    /// Model for Spotify playback state.
+    /// </summary>
     public class SpotifyPlayingState
     {
+        /// <summary>
+        /// Is playback currently playing.
+        /// </summary>
         public bool IsPlaying { get; set; }
+
+        /// <summary>
+        /// Currently playing track.
+        /// </summary>
         public SpotifyTrackModel? Track { get; set; }
     }
 }
--- api/SpotifyPlaylistJanitorAPI/Models/Spotify/SpotifyTrackModel.cs
+++ api/SpotifyPlaylistJanitorAPI/Models/Spotify/SpotifyTrackModel.cs
@@ -1,21 +1,48 @@
 ﻿namespace SpotifyPlaylistJanitorAPI.Models.Spotify
 {
+    /// <summary>
+    /// Model for Spotify Track.
+    /// </summary>
     public class SpotifyTrackModel
     {
+        /// <summary>
+        /// Spotify track id.
+        /// </summary>
         public string? Id { get; set; }
 
-        public string? PlaylistId { get; set; }
-
-        public string? ListeningOn { get; set; }
-
+        /// <summary>
+        /// Spotify track name.
+        /// </summary>
         public string? Name { get; set; }
 
-        public IEnumerable<SpotifyArtistModel> Artists { get; set; }
+        /// <summary>
+        /// Spotify playlist id.
+        /// </summary>
+        public string? PlaylistId { get; set; }
 
+        /// <summary>
+        /// List of Spotify artists associated with the track.
+        /// </summary>
+        public IEnumerable<SpotifyArtistModel> Artists { get; set; } = new List<SpotifyArtistModel>();
+
+        /// <summary>
+        /// Spotify album associated with the track.
+        /// </summary>
         public SpotifyAlbumModel? Album { get; set; }
 
+        /// <summary>
+        /// Length of the song in milliseconds.
+        /// </summary>
         public int? Duration { get; set; }
 
+        /// <summary>
+        /// Playback progress of the song in milliseconds.
+        /// </summary>
         public int? Progress { get; set; }
+
+        /// <summary>
+        /// Device track is being played on.
+        /// </summary>
+        public string? ListeningOn { get; set; }
     }
 }
