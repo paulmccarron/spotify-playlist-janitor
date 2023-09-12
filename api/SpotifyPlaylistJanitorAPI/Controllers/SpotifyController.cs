8ee0e905ad8ccc6f5f8f06d720f83b329d832d21
pmccarron <pmccarron@foundationmedicine.com>
2023-09-12 11:50:09 +0100

    Added endpoint to get spotify playlist by id Added unit tests Added swagger examples

--- api/SpotifyPlaylistJanitorAPI/Controllers/SpotifyController.cs
+++ api/SpotifyPlaylistJanitorAPI/Controllers/SpotifyController.cs
@@ -71,9 +71,45 @@ namespace SpotifyPlaylistJanitorAPI.Controllers
             return Ok(playlists);
         }
 
+        /// <summary>
+        /// Returns current user's playlists by id.
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Current user's playlist.</response>
+        /// <response code="404">No playlist found for that id.</response>
+        /// <response code="500">Application has not been logged into users Spotify account.</response>
+        [HttpGet("playlists/{id}")]
+        [ProducesResponseType(typeof(IEnumerable<SpotifyPlaylistModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponseModel), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorResponseModel), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(SpotifyPlaylistModelExample))]
+        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(SpotifyPlaylistNotFoundExample))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(ApplicationNotLoggedInExample))]
+        public async Task<ActionResult<SpotifyPlaylistModel>> GetPlaylist(string id)
+        {
+            if (!_spotifyService.IsLoggedIn)
+            {
+                return GetApplicationNotLoggedResponse();
+            }
+
+            var playlist = await _spotifyService.GetUserPlaylist(id);
+
+            if(playlist is null)
+            {
+                return PlaylistNotFoundResponse(id);
+            }
+
+            return Ok(playlist);
+        }
+
         private ActionResult GetApplicationNotLoggedResponse()
         {
             return StatusCode(500, new { Message = "Application has not been logged into your Spotify account." });
+        }
+
+        private NotFoundObjectResult PlaylistNotFoundResponse(string id)
+        {
+            return NotFound(new { Message = $"Could not find Spotify playlist with id: {id}" });
         }
     }
 }
---
+++ api/SpotifyPlaylistJanitorAPI/SwaggerExamples/Spotify/SpotifyPlaylistNotFoundExample.cs
@@ -0,0 +1,25 @@
+﻿using SpotifyPlaylistJanitorAPI.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SpotifyPlaylistJanitorAPI.SwaggerExamples.Spotify
+{
+    /// <summary>
+    /// Swagger example.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class SpotifyPlaylistNotFoundExample : IExamplesProvider<ErrorResponseModel>
+    {
+        /// <summary>
+        /// Implementation of Swashbuckle GetExamples() IExamplesProvider method.
+        /// </summary>
+        /// <returns>Example <see cref="ErrorResponseModel"/>ErrorResponseModel.</returns>
+        public ErrorResponseModel GetExamples()
+        {
+            return new ErrorResponseModel
+            {
+                Message = "Could not find Spotify playlist with id: ID",
+            };
+        }
+    }
+}
