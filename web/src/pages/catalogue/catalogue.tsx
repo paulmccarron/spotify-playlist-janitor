204c423d1591fcd5821473a1f5eef8b74c6fd2ee
Paul McCarron <paulcmccarron@gmail.com>
2023-10-07 23:32:36 +0100

    Added Select component

--- web/src/pages/catalogue/catalogue.tsx
+++ web/src/pages/catalogue/catalogue.tsx
@@ -2,10 +2,18 @@ import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { TextInput } from '../../shared/components/text-input';
 import { Button } from '../../shared/components/button';
+import { Select } from '../../shared/components/select';
+
+const options = [
+  {label: 'Option 1', value: 'option1'},
+  {label: 'Option 2', value: 'option2'},
+  {label: 'Option 3', value: 'option3'},
+]
 
 export const Catalogue = () => {
   const [textValue, setTextInputValue] = useState('');
   const [numberValue, setNumberInputValue] = useState<number | undefined>();
+  const [selectValue, setSelectValue] = useState('');
 
   const onTextChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setTextInputValue(e.target.value);
@@ -15,6 +23,10 @@ export const Catalogue = () => {
     const newValue = parseInt(e.target.value)
     setNumberInputValue(newValue);
   }, [setNumberInputValue])
+
+  const onSelectChange = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectValue(e.target.value);
+  }, [setSelectValue])
 
   return (
     <Content>
@@ -29,7 +41,12 @@ export const Catalogue = () => {
         </div>
         <div>
           <>Number Box</>
-          <TextInput {...{ type: 'number', placeholder: 'Enter number...', value: numberValue, onChange: onNumberChange }} /></div>
+          <TextInput {...{ type: 'number', placeholder: 'Enter number...', value: numberValue, onChange: onNumberChange }} />
+        </div>
+        <div>
+          <>Select</>
+          <Select {...{ value: selectValue, placeholder: 'Select option...', options, onChange: onSelectChange }} />
+        </div>
         <div>
           <>Button Primary</>
           <Button {...{ text: 'Primary', onClick: () => { alert('Primary Button Clicked!') } }} />
---
+++ web/src/shared/components/select/select.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type SelectProps = {
+    value: string;
+    placeholder?: string;
+    options: { label: string; value: string }[];
+    onChange(e: any): void;
+}
+
+export const Select = ({ value = '', placeholder = '', options = [], onChange }: SelectProps) => {
+    var mappedOptions = options.map(option => <option value={option.value}>{option.label}</option>)
+    return (
+        <Container>
+            <select {...{ value, placeholder, onChange }}>
+                {mappedOptions}
+            </select>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+background-color: white;
+padding: 0.4rem 1rem;
+border-radius: 2rem;
+display: flex;
+align-items: center;
+gap: 0.5rem;
+select {
+  background-color: white;
+  font-size: 1rem;
+  border: none;
+  height: 2rem;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+}
+`;
