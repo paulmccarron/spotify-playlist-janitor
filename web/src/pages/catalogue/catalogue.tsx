371c3d597d9f75a1f517f5ac3155e39028080675
pmccarron <pmccarron@foundationmedicine.com>
2023-10-10 13:06:34 +0100

    Moved data into file

--- web/src/pages/catalogue/catalogue.tsx
+++ web/src/pages/catalogue/catalogue.tsx
@@ -5,76 +5,8 @@ import { Button } from "../../shared/components/button";
 import { Select } from "../../shared/components/select";
 import { Toggle } from "../../shared/components/toggle";
 import { Tabs, Tab, TabPanel, TabList } from "../../shared/components/tabs";
-import { Column, Table } from "../../shared/components/table";
-import { VscTrash } from "react-icons/vsc";
-import { PiClock } from "react-icons/pi";
-
-const options = [
-  { label: "Option 1", value: "option1" },
-  { label: "Option 2", value: "option2" },
-  { label: "Option 3", value: "option3" },
-];
-
-const tableData = [
-  {
-    id: 1,
-    image: "https://i.scdn.co/image/ab67616d00004851835a9e77dae1c928f871ac73",
-    title: "Longshot",
-    artist: "Catfish and the Bottlemen",
-    album: "The Balance",
-    length: 232960,
-    date_skipped: new Date(2023, 9, 15, 15, 30, 45, 20),
-    delete: undefined,
-  },
-  {
-    id: 2,
-    image: "https://i.scdn.co/image/ab67616d0000485158406b3f1ac3ceaff7a64fef",
-    title: "Dark Necessities",
-    artist: "Red Hot Chili Peppers",
-    album: "The Getaway",
-    length: 302000,
-    date_skipped: new Date(2023, 9, 15, 15, 30, 45, 25),
-    delete: undefined,
-  },
-];
-
-const tableColumns: Column[] = [
-  {
-    label: "",
-    accessor: "image",
-    sortable: false,
-    render: (url: string) => <img alt="" width={40} height={40} src={url} />,
-  },
-  { label: "Title", accessor: "title", sortable: true, sortbyOrder: "desc" },
-  { label: "Artist", accessor: "artist", sortable: true },
-  { label: "Album", accessor: "album", sortable: true },
-  {
-    label: <PiClock />,
-    accessor: "length",
-    sortable: true,
-    render: (millis: number) => {
-      var minutes = Math.floor(millis / 60000);
-      var seconds = parseInt(((millis % 60000) / 1000).toFixed(0));
-      return seconds === 60
-        ? minutes + 1 + ":00"
-        : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-    },
-  },
-  {
-    label: "Date Skipped",
-    accessor: "date_skipped",
-    sortable: true,
-    render: (date: Date) => date.toISOString(),
-  },
-  {
-    label: "",
-    accessor: "delete",
-    sortable: false,
-    render: (id: string) => (
-      <VscTrash onClick={() => alert(`Deleted track id: ${id}`)} />
-    ),
-  },
-];
+import { Table } from "../../shared/components/table";
+import { selectOptions, tableColumns, tableData } from "./data";
 
 export const Catalogue = () => {
   const [textValue, setTextInputValue] = useState("");
@@ -173,7 +105,7 @@ export const Catalogue = () => {
                 value: selectValue,
                 label: "Select Label",
                 placeholder: "Select option...",
-                options,
+                options: selectOptions,
                 onChange: onSelectChange,
               }}
             />
